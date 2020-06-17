cd2e5ad2b7db9bcfa66cef29718cd3ccdea1f0b5
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-06-17 16:54:32 +0200

    prepa release

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 # coding: utf-8
 
 # Do not import unicode_literals it generate an error when install module with pip
-from __future__ import (print_function,
-                        absolute_import, division)
+from __future__ import print_function, absolute_import, division
 
 import os
 import re
@@ -17,28 +16,30 @@ def get_version(path="./VERSION"):
 
 
 setuptools.setup(
-    name='pypn_habref_api',
+    name="pypn_habref_api",
     version=get_version(),
     description="Python lib related to Habref referential (INPN)",
-    long_description=open('README.md', encoding="utf-8").read().strip(),
+    long_description=open("README.md", encoding="utf-8").read().strip(),
     author="Les parcs nationaux de France",
-    url='https://github.com/PnX-SI/Habref-api-module',
-    packages=setuptools.find_packages('src'),
-    package_dir={'': 'src'},
-    install_requires=list(open('requirements.txt', 'r')),
+    url="https://github.com/PnX-SI/Habref-api-module",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=list(open("requirements.txt", "r")),
     include_package_data=True,
     # cmdclass={'install_db': InstallDB},
-    entry_points='''
+    entry_points="""
         [console_scripts]
         install_habref_schema=pypn_habref_api.scripts.database:install_schema
-    ''',
+    """,
     zip_safe=False,
-    keywords='ww',
-    classifiers=['Development Status :: 1 - Planning',
-                 'Intended Audience :: Developers',
-                 'Natural Language :: English',
-                 'Programming Language :: Python :: 3.5',
-                 'Programming Language :: Python :: 3.6',
-                 'License :: OSI Approved :: GNU Affero General Public License v3',
-                 'Operating System :: OS Independent'],
+    keywords="ww",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
 )

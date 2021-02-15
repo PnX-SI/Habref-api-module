ce9577b74a76f6d137329309eadc30cd8ec3a971
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2021-02-15 16:50:20 +0100

    import db from environment variable

--- src/pypn_habref_api/env.py
+++ src/pypn_habref_api/env.py
@@ -1,5 +1,19 @@
+from os import environ
+from importlib import import_module
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-DB = SQLAlchemy()
+
 MA = Marshmallow()
+
+db_path = environ.get('FLASK_SQLALCHEMY_DB')
+if db_path:
+    db_module_name, db_object_name = db_path.rsplit('.', 1)
+    db_module = import_module(db_module_name)
+    DB = getattr(db_module, db_object_name)
+else:
+    DB = SQLAlchemy()
+
+
+__all__ = ['MA', 'DB']

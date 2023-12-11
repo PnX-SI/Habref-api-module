18ad51b7f5d08e463281a21fe7dd888ca53f11d6
Jacobe2169 <jacques.fize@ecrins-parcnational.fr>
2023-12-11 11:37:36 +0100

    drop .query

--- test_settings.py
+++ test_settings.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost/postgres"
+SQLALCHEMY_DATABASE_URI = "postgresql://geonatadmin:geonatadmin@localhost:5432/geonature2db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False

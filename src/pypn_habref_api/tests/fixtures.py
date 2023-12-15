25f027fe7b094c0c23bde4973f0f8c06d8e0850a
Jacobe2169 <jacques.fize@ecrins-parcnational.fr>
2023-12-15 12:04:17 +0100

    minor fix

--- src/pypn_habref_api/tests/fixtures.py
+++ src/pypn_habref_api/tests/fixtures.py
@@ -3,12 +3,13 @@ import pytest
 from pypn_habref_api.models import BibListHabitat, Habref
 from pypn_habref_api.env import db
 from .conftest import app
+from sqlalchemy import select
 
 
 @pytest.fixture(scope="session")
 def bib_list(app):
     new_list = BibListHabitat(list_name="list test")
-    habitat = db.session.execute(db.select(Habref).limit(1)).scalar_one()
+    habitat = db.session.execute(select(Habref).limit(1)).scalar_one()
     new_list.habitats.append(habitat)
     db.session.add(new_list)
     return new_list

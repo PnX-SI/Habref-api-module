393fb978477d8fa0810261ae1dc941cea9f7aec5
Pierre-Narcisi <pierre.narcisi@epitech.eu>
2025-12-03 11:04:58 +0100

    Update habref

---
+++ src/pypn_habref_api/commands/habref_v7.py
@@ -0,0 +1,226 @@
+from zipfile import ZipFile
+from collections import OrderedDict
+import logging
+
+from pypn_habref_api.models import BibListHabitat, Habref
+from pypn_habref_api.env import db
+
+from sqlalchemy.schema import Table, MetaData, PrimaryKeyConstraint
+
+import click
+from flask.cli import with_appcontext
+
+from alembic import op
+
+from utils_flask_sqla.migrations.utils import open_remote_file
+from .utils import copy_from_csv, empty_table, restore_constraints
+
+base_url = "https://geonature.fr/data/inpn/habitats/"
+table_files = {
+    "typoref": {
+        "filename": "TYPOREF_70.csv",
+        "table_fields": {
+            "cd_typo": "CD_TYPO",
+            "cd_table": "CD_TABLE",
+            "lb_nom_typo": "LB_NOM_TYPO",
+            "nom_jeu_donnees": "NOM_JEU_DONNEES",
+            "date_creation": "DATE_CREATION",
+            "auteur_typo": "AUTEUR_TYPO",
+            "auteur_table": "AUTEUR_TABLE",
+            "territoire": "TERRITOIRE",
+            "organisme": "ORGANISME",
+            "langue": "LANGUE",
+            "presentation": "PRESENTATION",
+            "description": "DESCRIPTION",
+            "origine": "ORIGINE",
+            "ref_biblio": "REF_BIBLIO",
+            "mots_cles": "MOTS_CLES",
+            "referencement": "REFERENCEMENT",
+            "diffusion": "DIFFUSION",
+            "derniere_modif": "DERNIERE_MODIF",
+            "type_table": "TYPE_TABLE",
+            "cd_typo_entre": "CD_TYPO_ENTRE",
+            "cd_typo_sortie": "CD_TYPO_SORTIE",
+        },
+    },
+    "bib_habref_typo_rel": {
+        "filename": "HABREF_TYPE_REL_70.csv",
+        "table_fields": {
+            "cd_type_rel": "CD_TYPE_REL",
+            "lb_type_rel": "LB_TYPE_REL",
+            "lb_rel": "LB_REL",
+            "corresp_hab": "CORRESP_HAB",
+            "corresp_esp": "CORRESP_ESP",
+            "corresp_syn": "CORRESP_SYN",
+            "date_crea": "DATE_CREA",
+            "date_modif": "DATE_MODIF",
+        },
+    },
+    "bib_habref_statuts": {
+        "filename": "HABREF_STATUTS.csv",
+        "table_fields": {
+            "statut": "STATUT",
+            "description": "DESCRIPTION",
+            "definition": "DEFINITION",
+            "ordre": "ORDRE",
+        },
+    },
+    "habref_sources": {
+        "filename": "HABREF_SOURCES_70.csv",
+        "table_fields": {
+            "cd_source": "CD_SOURCE",
+            "cd_doc": "CD_DOC",
+            "type_source": "TYPE_SOURCE",
+            "auteur_source": "AUTEUR_SOURCE",
+            "date_source": "DATE_SOURCE",
+            "lb_source": "LB_SOURCE",
+            "lb_source_complet": "LB_SOURCE_COMPLET",
+            "titre": "TITRE",
+            "link": "LINK",
+            "date_crea": "DATE_CREA",
+            "date_modif": "DATE_MODIF",
+        },
+    },
+    "habref": {
+        "filename": "HABREF_70.csv",
+        "table_fields": {
+            "cd_hab": "CD_HAB",
+            "fg_validite": "FG_VALIDITE",
+            "cd_typo": "CD_TYPO",
+            "lb_code": "LB_CODE",
+            "lb_hab_fr": "LB_HAB_FR",
+            "lb_hab_fr_complet": "LB_HAB_FR_COMPLET",
+            "lb_hab_en": "LB_HAB_EN",
+            "lb_auteur": "LB_AUTEUR",
+            "niveau": "NIVEAU",
+            "lb_niveau": "LB_NIVEAU",
+            "cd_hab_sup": "CD_HAB_SUP",
+            "path_cd_hab": "PATH_CD_HAB",
+            "france": "FRANCE",
+            "lb_description": "LB_DESCRIPTION",
+        },
+    },
+    "habref_corresp_hab": {
+        "filename": "HABREF_CORRESP_HAB_70.csv",
+        "table_fields": {
+            "cd_corresp_hab": "CD_CORRESP_HAB",
+            "cd_hab_entre": "CD_HAB_ENTRE",
+            "cd_hab_sortie": "CD_HAB_SORTIE",
+            "cd_type_relation": "CD_TYPE_RELATION",
+            "lb_condition": "LB_CONDITION",
+            "lb_remarques": "LB_REMARQUES",
+            "validite": "VALIDITE",
+            "cd_typo_entre": "CD_TYPO_ENTRE",
+            "cd_typo_sortie": "CD_TYPO_SORTIE",
+        },
+    },
+    "habref_corresp_taxon": {
+        "filename": "HABREF_CORRESP_TAXON_70.csv",
+        "table_fields": {
+            "cd_corresp_tax": "CD_CORRESP_TAX",
+            "cd_hab_entre": "CD_HAB_ENTRE",
+            "cd_nom": "CD_NOM",
+            "cd_type_relation": "CD_TYPE_RELATION",
+            "lb_condition": "LB_CONDITION",
+            "lb_remarques": "LB_REMARQUES",
+            "nom_cite": "NOM_CITE",
+            "validite": "VALIDITE",
+            "date_crea": "DATE_CREA",
+            "date_modif": "DATE_MODIF",
+        },
+    },
+    "cor_habref_terr_statut": {
+        "filename": "HABREF_TERR_70.csv",
+        "table_fields": {
+            "cd_hab_ter": "CD_HAB_TERR",
+            "cd_hab": "CD_HAB",
+            "cd_sig_terr": "CD_SIG_TERR",
+            "cd_statut_presence": "CD_STATUT_PRESENCE",
+            "date_crea": "DATE_CREA",
+            "date_modif": "DATE_MODIF",
+        },
+    },
+    "typoref_fields": {
+        "filename": "TYPOREF_FIELDS_70.csv",
+        "table_fields": {
+            "cd_hab_field": "CD_HAB_FIELD",
+            "cd_typo": "CD_TYPO",
+            "lb_hab_field": "LB_HAB_FIELD",
+            "format_hab_field": "FORMAT_HAB_FIELD",
+            "descript_hab_field": "DESCRIPT_HAB_FIELD",
+            "ordre_hab_field": "ORDRE_HAB_FIELD",
+            "length_hab_field": "LENGTH_HAB_FIELD",
+            "lb_label": "LB_LABEL",
+            "date_crea": "DATE_CREA",
+            "date_modif": "DATE_MODIF",
+        },
+    },
+    "cor_habref_description": {
+        "filename": "HABREF_DESCRIPTION_70.csv",
+        "table_fields": {
+            "cd_hab_description": "CD_HAB_DESCRIPTION",
+            "cd_hab": "CD_HAB",
+            "cd_hab_field": "CD_HAB_FIELD",
+            "cd_typo": "CD_TYPO",
+            "lb_code": "LB_CODE",
+            "lb_hab_field": "LB_HAB_FIELD",
+            "valeurs": "VALEURS",
+        },
+    },
+    "cor_hab_source": {
+        "filename": "HABREF_LIEN_SOURCES_70.csv",
+        "table_fields": {
+            "cd_hab_lien_source": "CD_HAB_LIEN_SOURCE",
+            "cd": "CD",
+            "type_lien": "TYPE_LIEN",
+            "cd_source": "CD_SOURCE",
+            "origine": "ORIGINE",
+            "date_crea": "DATE_CREA",
+            "date_modif": "DATE_MODIF",
+        },
+    },
+}
+
+
+def import_habref(logger, num_version, habref_archive_name):
+    with open_remote_file(base_url, habref_archive_name, open_fct=ZipFile) as archive:
+        for table, value in table_files.items():
+
+            constraints = empty_table(
+                table,
+                db,
+                schema="ref_habitats",
+            )
+            logger.info(f"Insert HABREF v{num_version} {table}…")
+            with archive.open(value["filename"]) as f:
+
+                copy_from_csv(
+                    f,
+                    table,
+                    value["table_fields"],
+                    encoding="UTF-8",
+                    delimiter=";",
+                    schema="ref_habitats",
+                    db=db,
+                )
+            restore_constraints(
+                table,
+                db,
+                constraints,
+                schema="ref_habitats",
+            )
+
+
+@click.command()
+@with_appcontext
+def import_v07():
+    logger = logging.getLogger()
+
+    import_habref(
+        logger,
+        num_version="07",
+        habref_archive_name="HABREF_70.zip",
+    )
+
+    logger.info("Committing…")
+    db.session.commit()
---
+++ src/pypn_habref_api/commands/utils.py
@@ -0,0 +1,126 @@
+from csv import DictReader
+from io import TextIOWrapper
+
+import sqlalchemy as sa
+from sqlalchemy import inspect as sa_inspect
+from sqlalchemy.schema import (
+    Table,
+    MetaData,
+    PrimaryKeyConstraint,
+    ForeignKeyConstraint,
+)
+
+
+def get_csv_field_names(f, encoding, delimiter):
+    if encoding == "WIN1252":  # postgresql encoding
+        encoding = "cp1252"  # python encoding
+    t = TextIOWrapper(f, encoding=encoding)
+    reader = DictReader(t, delimiter=delimiter)
+    field_names = reader.fieldnames
+    t.detach()  # avoid f to be closed on t garbage collection
+    f.seek(0)
+    return field_names
+
+
+"""
+Insert CSV file into specified table.
+If source columns are specified, CSV file in copied in a temporary table,
+then data restricted to specified source columns are copied in final table.
+"""
+
+
+def copy_from_csv(
+    f,
+    table_name,
+    table_fields,
+    schema="",
+    header=True,
+    encoding=None,
+    delimiter=None,
+    db=None,
+):
+    bind = db.session.get_bind()
+    metadata = MetaData(bind=bind)
+    engine = db.engine
+
+    table_fields_list = list(table_fields.values())
+    table_fields_key_list = list(table_fields.keys())
+
+    final_table_name = table_name
+    table_name = f"import_{table_name}"
+    field_names = get_csv_field_names(f, encoding=encoding, delimiter=delimiter)
+    field_names = list(map(lambda field_name: field_name.replace("\ufeff", ""), field_names))
+    table = Table(
+        table_name,
+        metadata,
+        *[sa.Column(c, sa.String) for c in map(str.lower, field_names)],
+        schema=schema,
+    )
+    table.create(bind=db.session.connection())
+
+    options = ["FORMAT CSV"]
+    if header:
+        options.append("HEADER")
+    if encoding:
+        options.append(f"ENCODING '{encoding}'")
+    if delimiter:
+        options.append(f"DELIMITER E'{delimiter}'")
+    options = ", ".join(options)
+    cursor = db.session.connection().connection.cursor()
+    cursor.copy_expert(
+        f"""
+        COPY {schema}.{table_name}
+        FROM STDIN WITH ({options})
+    """,
+        f,
+    )
+
+    testTable = Table(final_table_name, db.metadata, schema=schema, autoload_with=engine)
+
+    for col in testTable.columns:
+        if col.name in table_fields:
+            table_fields[col.name] = f"{table_fields[col.name]}::{col.type}"
+    table_fields_list = list(table_fields.values())
+
+    db.session.execute(
+        f"""
+    INSERT INTO {schema}.{final_table_name} ({", ".join(table_fields_key_list)})
+        SELECT {", ".join(table_fields_list)}
+        FROM {schema}.{table_name};
+    """
+    )
+    table.drop(bind=db.session.connection())
+
+
+def empty_table(table_name, db, schema=""):
+    inspector = sa_inspect(db.engine)
+
+    # Récupère les foreign keys
+    foreign_keys = inspector.get_foreign_keys(table_name, schema=schema)
+    for fk in foreign_keys:
+        constraint_name = fk["name"]
+        if constraint_name:  # Vérifie que le nom existe
+            db.session.execute(
+                f"ALTER TABLE {schema}.{table_name} DROP CONSTRAINT {constraint_name} CASCADE"
+            )
+    db.session.execute(f"TRUNCATE TABLE {schema}.{table_name} CASCADE")
+    return foreign_keys
+
+
+def restore_constraints(table_name, db, constraints, schema=""):
+    if constraints is None:
+        return
+    for constraint in constraints:
+        if isinstance(constraint, PrimaryKeyConstraint):
+            cols = ", ".join(str(col).split(".")[1] for col in constraint.columns)
+            db.session.execute(
+                f"ALTER TABLE {schema}.{table_name} ADD CONSTRAINT {constraint.name} UNIQUE ({cols})"
+            )
+        elif isinstance(constraint, ForeignKeyConstraint):
+            cols = ", ".join(str(col).split(".")[1] for col in constraint.columns)
+            ref_cols = ", ".join(
+                str(col).split(".")[1] for col in constraint.references[0].columns
+            )
+            db.session.execute(
+                f"ALTER TABLE {schema}.{table_name} ADD CONSTRAINT {constraint.name} FOREIGN KEY {cols} REFERENCES {constraint.references[0].table.name}({ref_cols})"
+            )

26d529da300c9a94651bd7c96f36a85fad66485f
Pierre-Narcisi <pierre.narcisi@epitech.eu>
2025-12-03 11:04:58 +0100

    Update habref

---
+++ setup.py
@@ -0,0 +1,50 @@
+import setuptools
+from pathlib import Path
+
+root_dir = Path(__file__).absolute().parent
+with (root_dir / "VERSION").open() as f:
+    version = f.read()
+with (root_dir / "README.md").open() as f:
+    long_description = f.read()
+with (root_dir / "requirements.in").open() as f:
+    requirements = f.read().splitlines()
+
+
+setuptools.setup(
+    name="pypn_habref_api",
+    version=version,
+    description="Python lib related to Habref referential (INPN)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    maintainer="Parcs nationaux des Écrins et des Cévennes",
+    maintainer_email="geonature@ecrins-parcnational.fr",
+    url="https://github.com/PnX-SI/Habref-api-module",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=requirements,
+    extras_require={
+        "tests": [
+            "pytest",
+            "pytest-flask",
+        ],
+    },
+    include_package_data=True,
+    entry_points={
+        "alembic": [
+            "migrations = pypn_habref_api.migrations:versions",
+        ],
+        "flask.commands": [
+            "habref = pypn_habref_api.commands.habref:habref",
+        ],
+    },
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+)
